4f7f77f6ac09655304a0b7db8eab681c9cf495bd
dai <kikuchi.dai@gmail.com>
2020-05-22 16:32:50 +0900

    Fix an issue

--- app/core/models.py
+++ app/core/models.py
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_stuff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     # assign the user manager to the object attributes
     objects = UserManager()
