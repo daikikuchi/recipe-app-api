3d383c0230c0391dedae33c2e31b390c2406cc45
dai <kikuchi.dai@gmail.com>
2020-05-23 17:39:45 +0900

    Updated admin to support user model.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,9 +24,28 @@ class AdminSiteTests(TestCase):
     def test_users_listed(self):
         """Test that users are listd on user page"""
         url = reverse('admin:core_user_changelist')
+        # print('url',url) /admin/core/user/
         # test client perform a HTTP GET on the url
         res = self.client.get(url)
+        # print('res',res)
+        # <TemplateResponse status_code=200, "text/html; charset=utf-8">
 
         # assertContains checks the http res is 200
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # /admin/core/user/1
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
